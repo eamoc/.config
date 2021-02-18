fb1fc9c367e83af32e4837d443e035fddebbfd06
Eamonn Costello <eamonncostello@gmail.com>
2021-02-18 19:47:56 +0000

    Deleted the old 'bash' direcotry

--- bash/ip6tables_ruleset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#Define a simple firewall that denies all incoming traffic but allows outgoing traffic
-sudo ip6tables -A INPUT -j REJECT
-sudo ip6tables -A FORWARD -j REJECT
-
-printf "Finished applying IPv6 ruleset\n\n"
